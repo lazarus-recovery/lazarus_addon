08546269cf42384f0025986ec8a475e2e9ff0466
matt wilkie <maphew@gmail.com>
2017-12-18 13:11:50 -0800

    dump old src files in prep for merge with current web-ext branch

--- src/chrome/content/update-expires.js
+++
@@ -1,6 +0,0 @@
-
-
-function toggleExpireSaveForms(checkbox){
-	document.getElementById("extensions-lazarus-expireSavedFormsInterval").disabled = !checkbox.checked;
-	document.getElementById("extensions-lazarus-expireSavedFormsUnit").disabled = !checkbox.checked;
-}
